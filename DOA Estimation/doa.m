cca08c99bf2e3368ef80d892bc3fea67a8087d31
George Chen <ghchen10@outlook.com>
2022-06-08 14:45:53 +0100

    ay

--- DOA Estimation/doa.m
+++ DOA Estimation/doa.m
@@ -1,16 +1,24 @@
 % TDOA method   
 
 delays = zeros(1, 8);
-
-for i=7:14
-    [s,Fs] = audioread(sprintf('Recordings/phase %d.wav',i));
+sum = 0.0;
+sum_sq = 0.0;
+sum_rmse = 0.0;
+for i=1:20
+    [s,Fs] = audioread(sprintf('Recordings/SE/SE%d.wav',i));
     audiowrite(sprintf('Recordings/phase %d_left.wav',i), s(:,1),Fs);
     audiowrite(sprintf('Recordings/phase %d_right.wav',i), s(:,2),Fs);
     x = audioread(sprintf('Recordings/phase %d_left.wav',i));
     y = audioread(sprintf('Recordings/phase %d_right.wav',i));
     delay = finddelay(x,y, 0.1*Fs); % positive delay implies left arrives before right
-    delays(i-6) = delay;
+    sum = sum + delay;
+    sum_sq = sum_sq + ((delay-23.6)^2);
 end
+
+delays(1) = sum/20;
+disp(delay);
+sum_rmse = sqrt(sum_sq/20);
+disp(sum_rmse)
 
 phase_delays = array2table(delays./Fs,"VariableNames",["Phase 7", "Phase 8", "Phase 9", "Phase 10", ...
     "Phase 11", "Phase 12", "Phase 13", "Phase 14"]);
@@ -19,13 +27,6 @@ disp(phase_delays); % theoretical max delay is 15.82 / 34300 = 0.00046122448
 
 angles = zeros(1, 8);
 angles(1) = delay_to_angle("Phase 7", phase_delays);
-angles(2) = delay_to_angle("Phase 8", phase_delays);
-angles(3) = delay_to_angle("Phase 9", phase_delays);
-angles(4) = delay_to_angle("Phase 10", phase_delays);
-angles(5) = delay_to_angle("Phase 11", phase_delays);
-angles(6) = delay_to_angle("Phase 12", phase_delays);
-angles(7) = delay_to_angle("Phase 13", phase_delays);
-angles(8) = delay_to_angle("Phase 14", phase_delays);
 
 phase_angles = array2table(angles,"VariableNames",["Phase 7", "Phase 8", "Phase 9", "Phase 10", ...
     "Phase 11", "Phase 12", "Phase 13", "Phase 14"]);
